f5f9c3608b3ca94c34e6575d4051f0a0dbd34edb
DallasThibodeau <Dthibodeau-cc@conestogac.on.ca>
2015-12-17 16:32:15 -0500

    Progress on background

---
+++ Assets/Scripts/scrollbg.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scrollbg : MonoBehaviour {
+
+    public float sizeofImg;
+    public float scrollSpeed;
+    private Vector3 startPosition;
+	// Use this for initialization
+	void Start () {
+        startPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float newPos = Mathf.Repeat(Time.time * scrollSpeed, sizeofImg);
+        transform.position = startPosition + Vector3.left * newPos;
+    }
+}

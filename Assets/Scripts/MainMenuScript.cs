4211c35928fc37f1d98f84d78f4a7efe0a4eb413
anthony-salutari <anthony17183@gmail.com>
2015-12-17 21:45:55 -0500

    Added menus including prefabs for mainmenu, pausemenu, aboutmenu, helpmenu

---
+++ Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuScript : MonoBehaviour {
+
+    public GameObject aboutMenu;
+    public GameObject helpMenu;
+
+	// Use this for initialization
+	void Start()
+    {
+        // pause the game time and wait for the user to select an option from the menu
+        Time.timeScale = 0;
+	}
+	
+	// Update is called once per frame
+	void Update()
+    {
+	
+	}
+
+    public void StartGame()
+    {
+        // hide the menu and start the game time
+        gameObject.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void About()
+    {
+        // hide this menu and show the about menu
+        gameObject.SetActive(false);
+        aboutMenu.SetActive(true);
+    }
+
+    public void Help()
+    {
+        // hide this menu and show the help menu
+        gameObject.SetActive(false);
+        helpMenu.SetActive(true);
+    }
+
+    public void QuitGame()
+    {
+        // quit the game
+        Application.Quit();
+    }
+}

4b562b951b333979eff7e41dd1423c844f6c8e40
anthony-salutari <anthony17183@gmail.com>
2015-12-17 22:08:26 -0500

    Added the game over menu

---
+++ Assets/Scripts/GameOverMenuScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameOverMenuScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start()
+    {
+        // pause the game
+        Time.timeScale = 0;
+	}
+	
+	// Update is called once per frame
+	void Update()
+    {
+	
+	}
+
+    public void RetryLevel()
+    {
+        // load the current level again and hide this menu
+        Application.LoadLevel(Application.loadedLevel);
+        gameObject.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        // quit game
+        Application.Quit();
+    }
+}

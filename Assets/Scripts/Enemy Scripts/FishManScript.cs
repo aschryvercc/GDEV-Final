b76d7f7ab1e99dac29538636b7c9091f18c43c4e
anthony-salutari <anthony17183@gmail.com>
2015-12-18 14:37:29 -0500

    Add sounds to enemy prefabs

---
+++ Assets/Scripts/Enemy Scripts/FishManScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FishManScript : MonoBehaviour {
+
+    // audio variables
+    private AudioSource[] audioSources;
+    private AudioSource spawnSound;
+    private AudioSource hitSound;
+    private AudioSource deathSound;
+
+	// Use this for initialization
+	void Start()
+    {
+        audioSources = GetComponents<AudioSource>();
+
+        spawnSound = audioSources[0];
+        hitSound = audioSources[1];
+        deathSound = audioSources[2];
+	}
+	
+	// Update is called once per frame
+	void Update()
+    {
+	
+	}
+
+    void playSound(AudioSource sound)
+    {
+        sound.Play();
+    }
+}
---
+++ Assets/Scripts/Enemy Scripts/WizardScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WizardScript : MonoBehaviour {
+
+    // audio variables
+    private AudioSource[] audioSources;
+    //private AudioSource spawnSound;
+    private AudioSource hitSound;
+    private AudioSource deathSound;
+
+	// Use this for initialization
+	void Start()
+    {
+        audioSources = GetComponents<AudioSource>();
+
+        hitSound = audioSources[0];
+        deathSound = audioSources[1];
+	}
+	
+	// Update is called once per frame
+	void Update()
+    {
+	
+	}
+
+    void playSound(AudioSource sound)
+    {
+        sound.Play();
+    }
+}

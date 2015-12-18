fec08faea587cae3792d341de72bc32ecdf07488
Aaron <aschryver-cc@conestogac.on.ca>
2015-12-18 01:55:19 -0500

    Basic left/right movement scripts done for player

---
+++ Assets/Scripts/Controller2D.cs
@@ -0,0 +1,124 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//[RequireComponent (typeof (BoxCollider2D))]
+public class Controller2D : MonoBehaviour {
+
+    public LayerMask collisionMask;
+
+    public int horizontalRayCount = 2;
+    public int verticalRayCount = 2;
+
+    float horizontalRaySpacing;
+    float verticalRaySpacing;
+
+    const float skinWidth = .015f;
+    public new BoxCollider2D collider;
+    RayCastOrigins rayCastOrigins;
+
+    // Use this for initialization
+    void Start()
+    {
+        //collider = gameObject.GetComponent<BoxCollider2D>();
+        CalculateRaySpacing();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateRaycastOrigins();
+        CalculateRaySpacing();
+
+        for (int i = 0; i < verticalRayCount; ++i)
+        {
+            Debug.DrawRay(rayCastOrigins.bottomLeft + Vector2.right * verticalRaySpacing * i, Vector2.up * -2, Color.red);
+        }
+
+    }
+
+    struct RayCastOrigins
+    {
+        public Vector2 topLeft, topRight;
+        public Vector2 bottomLeft, bottomRight;
+    }
+
+    public void Move(Vector3 velocity)
+    {
+        UpdateRaycastOrigins();
+        if(velocity.x != 0)
+        {
+            HorizontalCollisions(ref velocity);
+        }
+        if (velocity.y != 0)
+        {
+            VerticalCollisions(ref velocity);
+        }
+        transform.Translate(velocity);
+    }
+
+    void HorizontalCollisions(ref Vector3 velocity)
+    {
+        float directionX = Mathf.Sign(velocity.x);
+        float rayLength = Mathf.Abs(velocity.x) + skinWidth;
+
+        for (int i = 0; i < horizontalRayCount; i++)
+        {
+            Vector2 rayOrigin = (directionX == -1) ? rayCastOrigins.bottomLeft : rayCastOrigins.bottomRight;
+            rayOrigin += Vector2.up * (horizontalRaySpacing * i);
+            RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.right * directionX, rayLength, collisionMask);
+
+            Debug.DrawRay(rayOrigin, Vector2.right * directionX * rayLength, Color.red);
+
+            if (hit)
+            {
+                velocity.x = (hit.distance - skinWidth) * directionX;
+                rayLength = hit.distance;
+            }
+        }
+    }
+
+    void VerticalCollisions(ref Vector3 velocity)
+    {
+        float directionY = Mathf.Sign(velocity.y);
+        float rayLength = Mathf.Abs(velocity.y) + skinWidth;
+
+        for (int i = 0; i < verticalRayCount; i++)
+        {
+            Vector2 rayOrigin = (directionY == -1) ? rayCastOrigins.bottomLeft : rayCastOrigins.topLeft;
+            rayOrigin += Vector2.right * (verticalRaySpacing * i + velocity.x);
+            RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.up * directionY, rayLength, collisionMask);
+
+            Debug.DrawRay(rayOrigin, Vector2.up * directionY * rayLength, Color.red);
+
+            if (hit)
+            {
+                velocity.y = (hit.distance - skinWidth) * directionY;
+                rayLength = hit.distance;
+            }
+        }
+    }
+
+
+    void UpdateRaycastOrigins()
+    {
+        Bounds bounds = collider.bounds;
+        bounds.Expand(skinWidth * -2);
+
+        rayCastOrigins.bottomLeft = new Vector2(bounds.min.x, bounds.min.y);
+        rayCastOrigins.bottomRight = new Vector2(bounds.max.x, bounds.min.y);
+        rayCastOrigins.topLeft = new Vector2(bounds.min.x, bounds.max.y);
+        rayCastOrigins.topRight = new Vector2(bounds.max.x, bounds.max.y);
+    }
+
+    void CalculateRaySpacing()
+    {
+        Bounds bounds = collider.bounds;
+        bounds.Expand(skinWidth * -2);
+
+        horizontalRayCount = Mathf.Clamp(horizontalRayCount, 2, int.MaxValue);
+        verticalRayCount = Mathf.Clamp(verticalRayCount, 2, int.MaxValue);
+
+        horizontalRaySpacing = bounds.size.y / (horizontalRayCount - 1);
+        verticalRaySpacing = bounds.size.x / (verticalRayCount - 1);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,115 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+//[RequireComponent (typeof (Controller2D))]
+public class Player : MonoBehaviour {
+
+    float moveSpeed = 6;
+    public Controller2D controller;
+    float gravity = -20;
+    Vector3 velocity;
+    private bool faceRight = true;
+    public Animator animator;
+
+    void start()
+    {
+    }
+
+    void Update()
+    {
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        velocity.x = input.x * moveSpeed;
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        if (velocity.x > 0 || velocity.x < 0)
+        {
+            animator.SetBool("playerMoving", true);
+            animator.SetBool("playerStopRunning", false);
+            animator.SetBool("playerStartRunning", true);
+        }
+        else if (velocity.x == 0)
+        {
+            if (input.x == 0)
+            {
+                animator.SetBool("playerMoving", false);
+                animator.SetBool("playerStartRunning", false);
+                animator.SetBool("playerStopRunning", true);
+            }
+        }
+
+        if (velocity.x > 0 && !faceRight)
+        {
+            Flip();
+        }
+        if (velocity.x < 0 && faceRight)
+        {
+            Flip();
+        }
+    }
+
+    void Flip()
+    {
+        faceRight = !faceRight;
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+    }
+
+
+ //   public float restartLevelDelay = 1f;
+ //   public float speed;
+ //   public float jump;
+ //   private float moveVelocity;
+ //   bool grounded = true;
+ //   private Animator animator;
+
+	//// Use this for initialization
+	//protected override void Start ()
+ //   {
+ //       animator = GetComponent<Animator>();
+
+ //       base.Start();    
+	//}
+
+ //   private void OnDisable()
+ //   {
+
+ //   }
+
+ //   private void CheckIfGameOver()
+ //   {
+
+ //   }
+
+ //   protected override void AttemptMove<T>(int xDir, int yDir)
+ //   {
+ //       base.AttemptMove<T>(xDir, yDir);
+ //       RaycastHit2D hit;
+ //       CheckIfGameOver();
+ //   }
+
+ //   protected override void OnCantMove<T>(T component)
+ //   {
+
+ //   }
+
+ //   // Update is called once per frame
+ //   void Update ()
+ //   {
+ //       int horizontal = 0;
+ //       int vertical = 0;
+
+ //       horizontal = (int)Input.GetAxisRaw("Horizontal");
+ //       vertical = (int)Input.GetAxisRaw("Vertical");
+
+ //       //if (horizontal != 0 || vertical != 0)
+ //       //    AttemptMove<Wall>(horizontal, vertical);
+	//}
+}
